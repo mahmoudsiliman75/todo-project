64312034caa81a095d478219057c0ced4843fd71
MahmoudSiliman <modysiliman75@gmail.com>
2021-01-31 01:07:22 +0200

    change menu drawer background color

--- src/components/MenuDrawer.vue
+++ src/components/MenuDrawer.vue
@@ -1,23 +1,11 @@
 <template>
-  <v-navigation-drawer v-model="this.$store.state.drawerIsVisible" app class="grey lighten-4">
-    <!-- <v-list>
-      <v-list-tile>
-        <v-list-action>
-          <v-icon> mdi-dashboard </v-icon>
-        </v-list-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            <router-link to="/home" class="text-decoration-none black--text"> Dashboard </router-link>
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list> -->
+  <v-navigation-drawer v-model="this.$store.state.drawerIsVisible" app class="yellow darken-1">
 
     <v-list flat>
       <v-list-item-group>
 
         <!-- START:: LIST ITEM LINKS -->
-        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+        <v-list-item class="white--text" v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-icon>
             <v-icon> {{ link.icon }} </v-icon>
           </v-list-item-icon>
