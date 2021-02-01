e6a90a98e2f48a3ad9dea34e97e7b9ae9adf95bf
MahmoudSiliman <modysiliman75@gmail.com>
2021-02-02 00:30:06 +0200

    sort projects data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,18 @@
     <h1 class="title grey--text"> Home Page </h1>
 
     <v-container class="my-4">
+      <v-row class="px-3 mb-3">
+        <v-btn text tile small color="grey" @click="sortBy('title')">
+          <v-icon left> mdi-folder </v-icon>
+          <span class="caption grey--text"> By Project Name </span>
+        </v-btn>
+
+        <v-btn text tile small color="grey" @click="sortBy('person')">
+          <v-icon left> mdi-account </v-icon>
+          <span class="caption grey--text"> By Name </span>
+        </v-btn>
+      </v-row>
+
       <v-card :class="`project pa-3 my-3 ${project.status}`" v-for="project in projects" :key="project.title">
         <v-row>
           <v-col cols="12" md="6">
@@ -35,12 +47,17 @@ export default {
   data() {
     return {
     projects: [
-        { title: 'Create Vuetify App', person: 'Mahmoud Siliman', due: '5th May 2021', status: 'Ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
-        { title: 'Create Laravel Website', person: 'Mohamed Eid', due: '15th May 2021', status: 'Ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
-        { title: 'Create php Website', person: 'Mohamed Eid', due: '21th Jan 2021', status: 'Complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
-        { title: 'Create Vue Website', person: 'Mahmoud Siliman', due: '3rd Mars 2021', status: 'Overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
+        { title: 'Vuetify App', person: 'Mahmoud Siliman', due: '5th May 2021', status: 'Ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
+        { title: 'Laravel Website', person: 'Mohamed Eid', due: '15th May 2021', status: 'Ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
+        { title: 'php Website', person: 'Mohamed Eid', due: '21th Jan 2021', status: 'Complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
+        { title: 'Vue Website', person: 'Mahmoud Siliman', due: '3rd Mars 2021', status: 'Overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eos quasi, nam asperiores possimus ad placeat vel id facilis nemo maxime, odit vero enim suscipit aut ea ipsam aspernatur explicabo.' },
       ],
     };
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort( (a,b) => a[prop] < b[prop] ? -1 : 1 );
+    } 
   },
 };
 </script>
